e3b28dd52657c6c5ddf31743152d7a461497b774
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-05-14 19:25:19 -0400

    ajustando a lógica

--- total-projeto.py
+++ total-projeto.py
@@ -7,7 +7,7 @@ quantidade_atividadesB = int(input("Informe os dias para a Atividade B: "))
 quantidade_atividadesC = int(input("Informe os dias para a Atividade C: "))
 
 
-if quantidade_atividadesA and quantidade_atividadesB and quantidade_atividadesC < 0:
+if quantidade_atividadesA <0 or quantidade_atividadesB <0 or quantidade_atividadesC < 0:
     print("Os dias nao podem ser negativos")
 else:
     total_dias = quantidade_atividadesA + quantidade_atividadesB + quantidade_atividadesC
