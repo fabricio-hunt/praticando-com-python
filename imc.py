620f99975153984bc959a2a55f8cf3ba771216ba
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-05-16 23:28:40 -0400

    uodate

--- imc.py
+++ imc.py
@@ -1,5 +1,3 @@
-#Crie um programa que receba o peso (em kg) e a altura (em metros) e calcule o IMC usando a fórmula: IMC = peso / (altura ** 2) Depois, exiba o valor do IMC e uma mensagem indicando se está abaixo do peso (IMC < 18.5), peso normal (18.5 <= IMC < 25) ou acima do peso (IMC >= 25).
-
 peso = float(input("Digite o peso em kg: "))
 altura = float(input("Digite a altura em metros: "))
 
